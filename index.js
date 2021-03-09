505671b6c665f7d0271825779d4d55b0e5725510
nathanielgcowan <nathaniel.g.cowan@gmail.com>
2021-03-09 12:36:19 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -13,6 +13,10 @@ let walkToPark = function(dogName, dogBreed){
     return `Walk to the park with ${dogName} the ${dogBreed}`
 }
 
+let throwFrisbee = function(dogName, dogBreed){
+    console.log(`Walk to the park with ${dogName} the ${dogBreed}`);
+    return `Walk to the park with ${dogName} the ${dogBreed}`
+}
 const routine =[wakeDog, leashDog, walkToPark]
 
 function exerciseDog(dogName, dogBreed) {
