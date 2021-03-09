5107c91f50e0d1209b3b6f662d5e5ab0a853f188
nathanielgcowan <nathaniel.g.cowan@gmail.com>
2021-03-09 12:34:06 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -4,8 +4,8 @@ let wakeDog = function(dogName, dogBreed){
 }
 
 let leashDog = function(dogName, dogBreed){
-    console.log(`Wake ${dogName} the ${dogBreed}`);
-    return `Wake ${dogName} the ${dogBreed}`
+    console.log(`Leash ${dogName} the ${dogBreed}`);
+    return `Wak ${dogName} the ${dogBreed}`
 }
 
 const routine =[wakeDog, leashDog]
