1ef17607177260715801e2e2a3da82b130dec7a4
nathanielgcowan <nathaniel.g.cowan@gmail.com>
2021-03-09 12:31:32 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,26 +1,6 @@
-var wakeDog = function(dogName="Byron", dogBreed="poodle") {
-  console.log(`Wake ${dogName} the ${dogBreed}`);
-  return `Wake ${dogName} the ${dogBreed}`
-}
-
-function leashDog(dogName, dogBreed) {
-  console.log(`Wake ${dogName} the ${dogBreed}`);
-}
-
-function walkToPark(dogName, dogBreed) {
-  console.log(`Wake ${dogName} the ${dogBreed}`);
-}
-
-function throwFrisbee(dogName, dogBreed) {
-  console.log(`Wake ${dogName} the ${dogBreed}`);
-}
-
-function walkHome(dogName, dogBreed) {
-  console.log(`Wake ${dogName} the ${dogBreed}`);
-}
-
-function unleashDog(dogName, dogBreed) {
-  console.log(`Wake ${dogName} the ${dogBreed}`);
+let wakeDog = function(dogName, dogBreed){
+    console.log('Wake ${dogName} the ${dogBreed}');
+    return 'Wake ${dogName} the ${dogBreed}'
 }
 
 
